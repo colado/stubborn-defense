944345aab83ddcfe57de57c6b5e27168f99a1f80
Sergio Colado <33748103+colado@users.noreply.github.com>
2026-07-08 12:44:58 +0300

    Add scripts to player pieces

--- scripts/player_piece.gd
+++ scripts/player_piece.gd
@@ -1,10 +1,12 @@
-extends Node3D
+extends Area3D
+class_name PlayerPieceBase
 
 var move_tween: Tween;
 
+signal piece_selected(int)
+
 func _ready() -> void:
-    # Called when the node enters the scene tree
-    _initialize()
+    pass
 
 func move_to(target: Vector2, target_board_cell: String, duration: float = 0.2) -> void:
     if move_tween:
@@ -19,3 +21,21 @@ func move_to(target: Vector2, target_board_cell: String, duration: float = 0.2) 
     move_tween.finished.connect(func():
         on_move_finished(target_board_cell)
     )
+
+func on_move_finished(target_board_cell: String):
+    # update board and main
+    # 	board.MovePiece(currentBoardCell, targetBoardCell, unit, true);
+    # currentBoardCell = targetBoardCell;
+    # if (this is Pawn) CanPromote();
+    # main.UseMoves(1);
+    # EmitSignal(SignalName.PlayerMoved);
+    # if (main.MovesLeft == 0)
+    # {
+    # 	main.ResetMoves();
+    # }
+    pass
+
+func _input_event(camera, event, position, normal, shape_idx) -> void:
+    if (event is InputEventMouseButton or event is InputEventScreenTouch) and event.pressed:
+        print("reached ", get_instance_id())
+        piece_selected.emit(self)
