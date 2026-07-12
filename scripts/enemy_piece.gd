09260b7bd2d09a062f9f2d240d4f3b7a68d96e90
Sergio Colado <33748103+colado@users.noreply.github.com>
2026-07-12 12:29:54 +0300

    Create Enemy Piece script

---
+++ scripts/enemy_piece.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+var current_tile: String
+var piece_type: int
+var move_tween: Tween
+
+func _ready() -> void:
+	current_tile = GameState.board.get_nearest_tile(global_position)
+
+func move_to(target: Vector3, target_board_tile: String, duration: float = 0.2) -> void:
+	# Make sure vertical position is not changed
+	var normalized_target_location = Vector3(target.x, position.y, target.z)
+	if move_tween:
+		move_tween.kill()
+
+	move_tween = create_tween()
+
+	move_tween.tween_property(self, "global_position", normalized_target_location, duration) \
+		.set_trans(Tween.TransitionType.TRANS_SINE) \
+		.set_ease(Tween.EaseType.EASE_OUT)
+
+	move_tween.finished.connect(func():
+		on_move_finished(target_board_tile)
+	)
+
+func on_move_finished(coord: String):
+	current_tile = coord
